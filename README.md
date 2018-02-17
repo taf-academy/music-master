f9c3cca65eacc6243fd743aa057d866f90c961fd
Craig Robinson <realcraig@gmail.com>
2018-02-17 13:07:11 -0800

    initial template code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # music-master
 Making music with electronics
+
+## Links
+
+[Web Audio API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API)
---
+++ js/main.js
@@ -0,0 +1,26 @@
+var MusicMaster = (function () {
+	var mm = {
+            context: new AudioContext(),
+            oscilator: null
+        };
+
+    /**
+     * Play a note of given frequency
+     */   
+	mm.playNote = function(frequency) {
+        this.oscilator = this.context.createOscillator();
+        this.oscilator.type = "sine";
+        this.oscilator.frequency = frequency;
+        this.oscilator.connect(this.context.destination);
+        this.oscilator.start();
+    };
+
+    mm.stop = function() {
+        if (this.oscilator) {
+            this.oscilator.stop();
+        };
+    };
+
+
+	return mm;
+}());
